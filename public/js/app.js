536366324da5712883b8c5babf7b68eabb684542
arrancareye <arrancareye@gmail.com>
2017-03-21 21:08:06 +0600

    voting functionality, bootstrap based templating, component lifecycle, state instead of props

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,100 @@
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+    this.handleDownVote = this.handleDownVote.bind(this);
+  }
+  handleUpVote(){
+    this.props.onVote(this.props.id);
+  }
+  handleDownVote(){
+    this.props.onUnVote(this.props.id);
+  }
   render() {
     return (
-      <div className='column'>
-        <div className="ui card">
-          <div className="content">
-            <div className="right floated meta">14h</div>
-            <a href={this.props.url}><img className="ui avatar image" src={this.props.submitterAvatarUrl} /> </a>
-            {this.props.title}
-          </div>
-          <div className="image">
-            <img src={this.props.productImageUrl} />
-          </div>
-          <div className="content">
-            <span className="right floated">
-              <i className="heart outline like icon"></i>
-              {this.props.votes}likes
-            </span>
-            <i className="comment icon"></i>
-            3 comments
-          </div>
-          <div className="extra content">
-            <p>{this.props.description}</p>
-          </div>
-          <div className="extra content">
-            <div className="ui large transparent left icon input">
-              <i className="heart outline icon"></i>
-              <input type="text" placeholder="Add Comment..." />
-            </div>
-          </div>
+      <div className="col-md-4">
+        <div className="thumbnail">
+          <img src={this.props.imageUrl} />
         </div>
+        <div className="caption">
+          <h3>{this.props.title}</h3>
+          <p className="">{this.props.description}</p>
+          <button className="btn btn-success" onClick={this.handleDownVote}><i className="glyphicon glyphicon-thumbs-down" /></button>
+          <button className="btn btn-primary" onClick={this.handleUpVote}><i className="glyphicon glyphicon-thumbs-up" /></button>
+          <p className="pull-right">{this.props.votes} <i className="glyphicon glyphicon-heart" /></p>
+        </div>
+        <div className="input-group">
+            <input type="text" className="form-control" placeholder="Write comment..." />
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="button">Send!</button>
+            </span>
+          </div>
       </div>
-    );
+    )
   }
 }
 
 class ProductList extends React.Component {
-	  render(){
-	  	const products = Seed.products.sort((a, b)=>(
-	  		a.votes - b.votes
-	  	));
-	  	const productComponents = products.map((product)=>(
-	  		<Product
-	  			key={'product_' + product.id}
-					id={product.id}
-					title={product.title}
-					description={product.description}
-					url={product.url}
-					votes={product.votes}
-					submitterAvatarUrl={product.submitterAvatarUrl}
-					productImageUrl={product.productImageUrl}
-				/>
-	  	));
-		return(
-			<div className = "ui four column grid">
-				{productComponents}
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      products : []
+    }
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    this.handleProductDownVote = this.handleProductDownVote.bind(this);
+  }
+  componentDidMount() {
+    this.setState({products : Seed.products});
+  }
+  handleProductUpVote(productId) {
+    console.log(productId + 'st element was upvoted');
+    console.log(this);
+    let newProducts = this.state.products.map((product)=>{
+      if(product.id === productId){
+        return Object.assign({}, product, {votes: product.votes + 1});
+      } else{
+        return product;
+      }
+    });
+    this.setState({products : newProducts});
+  }
+  handleProductDownVote(productId) {
+    console.log(productId + 'st element was upvoted');
+    console.log(this);
+    let newProducts = this.state.products.map((product)=>{
+      if(product.id === productId){
+        return Object.assign({}, product, {votes: product.votes - 1});
+      } else{
+        return product;
+      }
+    });
+    this.setState({products : newProducts});
+  }
+  render() {
+    const products = this.state.products.sort((a, b)=>(
+      b.votes - a.votes
+    ));
+    let productsList = products.map((product, i)=>(
+      <Product key={product.id}
+               id={product.id}
+               title={product.title}
+               description={product.description}
+               imageUrl={product.productImageUrl}
+               votes={product.votes}
+               onVote={this.handleProductUpVote}
+               onUnVote={this.handleProductDownVote}
+      />
+    ))
+    return (
+      <div>
+        <h2>Products list</h2>
+        {productsList}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-	<ProductList />,
-	document.getElementById('content')
+  <ProductList />,
+  document.getElementById('mounter')
 )
